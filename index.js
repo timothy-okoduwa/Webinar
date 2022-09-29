bf861dc7c33bb92532b1e8cdc0598921e5bcd74b
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2022-09-29 14:53:58 +0100

    java

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("demo").innerHTML =  '<img src="./Assets/images/alarm.png" alt="">' +`     `+`   ` +`    `+days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
+ 
 
   // If the count down is finished, write some text
   if (distance < 0) {

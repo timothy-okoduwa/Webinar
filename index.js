c7e38037858decb918aa5279e52c2a655bf3382a
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-01-26 14:32:31 +0100

    seminar date

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 
-var countDownDate = new Date('jan 31, 2023 12:36:00').getTime();
+var countDownDate = new Date('february 8, 2023 16:00:00').getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
